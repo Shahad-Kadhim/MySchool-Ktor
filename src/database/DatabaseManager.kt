848165ad5745cca17fcb18753fd50c0364e7a5cf
Shahad Kadhim <almuhandisa.sh@gmail.com>
2022-01-26 18:48:04 +0300

    deploy

--- src/database/DatabaseManager.kt
+++ src/database/DatabaseManager.kt
@@ -5,11 +5,18 @@ import org.jetbrains.exposed.sql.Database
 
 class DatabaseManager {
 
+
+    private val host= System.getenv("DB_HOST")
+    private val port= System.getenv("DB_PORT")
+    private val databaseName= System.getenv("DB_NAME")
+    private val databaseUserName= System.getenv("DB_USER")
+    private val databasePassword= System.getenv("DB_PASSWORD")
+
     private val database = Database.connect(
-        url = "jdbc:mysql://localhost:3306/my_school",
-        driver = "org.h2.Driver",
-        user = "root",
-        password = ""
+        url = "jdbc:postgresql://$host:$port/$databaseName",
+        driver = "org.postgresql.Driver",
+        user = databaseUserName,
+        password = databasePassword
     )
 
     fun getDatabase() = database

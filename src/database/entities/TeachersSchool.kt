5944bc4011d51300bcd6fbf58dc4acd4a7d086a3
Shahad Kadhim <almuhandisa.sh@gmail.com>
2022-02-18 03:05:57 +0300

    edit teacher school relation

--- src/database/entities/TeachersSchool.kt
+++ src/database/entities/TeachersSchool.kt
@@ -4,8 +4,8 @@ import org.jetbrains.exposed.sql.Table
 
 
 object TeachersSchool: Table("teacher-school") {
-    val teacherId =( varchar("teacherId",50) references Teachers.id).primaryKey()
-    val schoolId = (varchar("schoolId",50) references Schools.id).primaryKey()
+    val teacherId =( varchar("teacherId",50).primaryKey() references Teachers.id)
+    val schoolId = (varchar("schoolId",50).primaryKey() references Schools.id)
     val dateJoined = long("dateJoined")
 
 
